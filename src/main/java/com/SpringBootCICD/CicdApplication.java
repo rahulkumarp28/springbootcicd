7bf01dc22aaf66b4597fe6f758e74a4071f8a739
Rahul.Kumar8 <rahul.kumar8@cognizant.com>
2024-01-14 22:24:32 +0530

    Initial commit

---
+++ src/main/java/com/SpringBootCICD/CicdApplication.java
@@ -0,0 +1,25 @@
+package com.SpringBootCICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/api/v1")
+public class CicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+	@GetMapping("/message")
+	public String getMessage() {
+
+		return "Life is Beautiful,Don't ruin it.";
+
+	}
+
+}
